66f7c4d8b14e31e80ec1c4efb35648a608664f83
Dylan Chamoulaud <dylan3396@hotmail.fr>
2019-03-28 16:21:10 +0100

    Cleaning project

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,60 +1,24 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
-
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+import React from "react";
+import {createBottomTabNavigator, createStackNavigator} from "react-navigation";
+import HomeScreen from "../screens/HomeScreen";
+import TabBarIcon from "../components/TabBarIcon";
 
 const HomeStack = createStackNavigator({
-  Home: HomeScreen,
+	Home: HomeScreen,
+}, {
+	headerMode: 'none',
+	navigationOptions: {
+		headerVisible: false,
+		tabBarIcon: (props) => <TabBarIcon {...props} name="md-home"/>
+
+	}
 });
-
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
-};
-
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
-});
-
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
-    />
-  ),
-};
-
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
-});
-
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
-    />
-  ),
-};
 
 export default createBottomTabNavigator({
-  HomeStack,
-  LinksStack,
-  SettingsStack,
+	HomeStack,
+},{
+	tabBarOptions: {
+		showLabel:false,
+		showIcon: true,
+	}
 });
